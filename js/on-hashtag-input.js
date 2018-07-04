4dbbfda5aa6c12635a3ccbb496f32cd77477aae3
Антон <bozhan89@gmail.com>
2018-07-04 12:17:42 +0300

    Начал править валидатор хэштегов

--- js/on-hashtag-input.js
+++ js/on-hashtag-input.js
@@ -5,50 +5,46 @@
   var HASHTAG_MAX_LENGTH = 20;
   var HASHTAG_MAX_COUNT = 5;
 
-  var hashErrorCount;
-  var setErrorCount = function (index) {
-    hashErrorCount = index + 1;
+  var hashtagTest = function (testedArray) {
+    switch (false) {
+
+      case testedArray.length <= HASHTAG_MAX_COUNT:
+        return 'Не больше 5 хэштегов';
+        break;
+
+      case testedArray.every(function (currentValue) {
+        return currentValue[0] === HASH_SYMBOL || currentValue[0] === undefined;
+      }):
+        return 'Начни все хэштеги с решетки!';
+        break;
+
+      case testedArray.every(function (currentValue) {
+        return currentValue !== HASH_SYMBOL;
+      }):
+        return 'Решетка очень одинока!';
+        break;
+
+      case testedArray.some(function (currentValue) {
+        return currentValue.length <= HASHTAG_MAX_LENGTH;
+      }):
+        return 'Хэштегам не надо быть длиннее 20 символов';
+        break;
+
+      case testedArray.every(function (currentValue, index, array) {
+        return array.lastIndexOf(currentValue) === index;
+      }):
+        return 'Пусть все хэштеги будут разными!';
+        break;
+
+      default:
+        return '';
+    }
   };
 
   window.onHashtagInput = function (event) {
     var target = event.target;
-    var hashtagsArray = target.value.split(' ');
-
-    switch (false) {
-
-      case hashtagsArray.every(function (currentValue, index) {
-        setErrorCount(index);
-        return currentValue[0] === HASH_SYMBOL || currentValue[0] === undefined;
-      }):
-        target.setCustomValidity('Начни ' + hashErrorCount + '-й хэштег с решетки!');
-        break;
-
-      case hashtagsArray.every(function (currentValue, index) {
-        setErrorCount(index);
-        return currentValue !== HASH_SYMBOL;
-      }):
-        target.setCustomValidity('У хэштэга ' + hashErrorCount + ' решетка очень одинока!');
-        break;
-
-      case hashtagsArray.some(function (currentValue, index) {
-        setErrorCount(index);
-        return currentValue.length <= HASHTAG_MAX_LENGTH;
-      }):
-        target.setCustomValidity(hashErrorCount + '-й хэштег слишком длинный');
-        break;
-
-      case hashtagsArray.every(function (currentValue, index, array) {
-        return array.lastIndexOf(currentValue) === index;
-      }):
-        target.setCustomValidity('Пусть все хэштеги будут разными!');
-        break;
-
-      case hashtagsArray.length <= HASHTAG_MAX_COUNT:
-        target.setCustomValidity('Не больше 5 хэштегов');
-        break;
-
-      default:
-        target.setCustomValidity('');
-    }
+    var hashtagsArray = target.value.split(/\s+/);
+    var errorMessage = hashtagTest(hashtagsArray);
+    target.setCustomValidity(errorMessage);
   };
 })();
